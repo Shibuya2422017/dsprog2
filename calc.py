45115d2e1d707f6af1055bb74e1a54a40d4346b0
Shibuya2422017 <shibuyakaito5224@gmail.com>
2024-11-25 17:36:16 +0900

    電卓の改造

---
+++ calc.py
@@ -0,0 +1,237 @@
+
+import flet as ft
+import math
+import random
+
+
+
+class CalcButton(ft.ElevatedButton):
+    def __init__(self, text, button_clicked, expand=1):
+        super().__init__()
+        self.text = text
+        self.expand = expand
+        self.on_click = button_clicked
+        self.data = text
+
+
+class DigitButton(CalcButton):
+    def __init__(self, text, button_clicked, expand=1):
+        CalcButton.__init__(self, text, button_clicked, expand)
+        self.bgcolor = ft.colors.WHITE24
+        self.color = ft.colors.WHITE
+
+
+class ActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.colors.ORANGE
+        self.color = ft.colors.WHITE
+
+
+class ExtraActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.colors.BLUE_GREY_100
+        self.color = ft.colors.BLACK
+
+
+class CalculatorApp(ft.Container):
+    # application's root control (i.e. "view") containing all other controls
+    def __init__(self):
+        super().__init__()
+        self.reset()
+
+        self.result = ft.Text(value="0", color=ft.colors.WHITE, size=20)
+        self.width = 650
+        self.bgcolor = ft.colors.BLACK
+        self.border_radius = ft.border_radius.all(20)
+        self.padding = 20
+        self.content = ft.Column(
+            controls=[
+                ft.Row(controls=[self.result], alignment="end"),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="mr", button_clicked=self.button_clicked),
+                        ExtraActionButton(
+                            text="AC", button_clicked=self.button_clicked
+                        ),
+                        ExtraActionButton(
+                            text="+/-", button_clicked=self.button_clicked
+                        ),
+                        ExtraActionButton(text="%", button_clicked=self.button_clicked),
+                        ActionButton(text="/", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="10*", button_clicked=self.button_clicked),
+                        DigitButton(text="7", button_clicked=self.button_clicked),
+                        DigitButton(text="8", button_clicked=self.button_clicked),
+                        DigitButton(text="9", button_clicked=self.button_clicked),
+                        ActionButton(text="*", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="log10", button_clicked=self.button_clicked),
+                        DigitButton(text="4", button_clicked=self.button_clicked),
+                        DigitButton(text="5", button_clicked=self.button_clicked),
+                        DigitButton(text="6", button_clicked=self.button_clicked),
+                        ActionButton(text="-", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="EE", button_clicked=self.button_clicked),
+                        DigitButton(text="1", button_clicked=self.button_clicked),
+                        DigitButton(text="2", button_clicked=self.button_clicked),
+                        DigitButton(text="3", button_clicked=self.button_clicked),
+                        ActionButton(text="+", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="Deg", button_clicked=self.button_clicked),
+                        DigitButton(text="Rand", button_clicked=self.button_clicked),
+                        DigitButton(
+                            text="0", expand=1, button_clicked=self.button_clicked
+                        ),
+                        DigitButton(text=".", button_clicked=self.button_clicked),
+                        ActionButton(text="=", button_clicked=self.button_clicked),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, e):
+        data = e.control.data
+        print(f"Button clicked with data = {data}")
+        if self.result.value == "Error" or data == "AC":
+            self.result.value = "0"
+            self.reset()
+
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.result.value == "0" or self.new_operand == True:
+                self.result.value = data
+                self.new_operand = False
+            else:
+                self.result.value = self.result.value + data
+
+        elif data in ("+", "-", "*", "/", "mr", "log10", "10*", "EE", "Deg", "Rand"):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
+            self.operator = data
+            if self.result.value == "Error":
+                self.operand1 = "0"
+            else:
+                self.operand1 = float(self.result.value)
+            self.new_operand = True
+
+        elif data in ("="):
+            self.result.value = self.calculate(
+                self.operand1, float(self.result.value), self.operator
+            )
+            self.reset()
+
+        elif data in ("%"):
+            self.result.value = float(self.result.value) / 100
+            self.reset()
+
+        elif data in ("+/-"):
+            if float(self.result.value) > 0:
+                self.result.value = "-" + str(self.result.value)
+
+            elif float(self.result.value) < 0:
+                self.result.value = str(
+                    self.format_number(abs(float(self.result.value)))
+                )
+        
+        elif data in ("mr"):
+            self.result.value = self.memory
+            self.reset()
+        
+        elif data in ("log10"):
+            self.result.value = self.format_number(math.log10(float(self.result.value)))
+            self.reset()
+        
+        elif data in ("10*"):
+            self.result.value = self.format_number(10 ** float(self.result.value))
+            self.reset()
+        
+        elif data in ("EE"):
+            self.result.value = self.result.value + "e"
+            self.reset()
+        
+        elif data in ("Deg"):
+            self.result.value = self.format_number(math.degrees(float(self.result.value)))
+            self.reset()
+        
+        elif data in ("Rand"):
+            self.result.value = self.format_number(random.random())
+            self.reset()
+        
+
+
+        self.update()
+
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+        
+        elif operator == "mr":
+            self.memory = operand2
+            return self.format_number(operand1)
+        
+        elif operator == "log10":
+            return self.format_number(math.log10(operand2))
+        
+        elif operator == "10*":
+            return self.format_number(10 ** operand2)
+        
+        elif operator == "EE":
+            return self.format_number(operand2)
+        
+        elif operator == "Deg":
+            return self.format_number(math.degrees(operand2))
+        
+        elif operator == "Rand":
+            return self.format_number(random.random())
+        
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
+
+
+def main(page: ft.Page):
+    page.title = "Calc App"
+    # create application instance
+    calc = CalculatorApp()
+
+    # add application's root control to the page
+    page.add(calc)
+
+
+ft.app(target=main)
+
